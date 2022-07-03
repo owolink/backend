2bed889b865a513ab3d6e5906c18497dfe7bb0ab
ustinov.ivan565 <ustinov.ivan565@gmail.com>
2022-07-03 13:08:14 +0300

    fixed import

--- category/models.py
+++ category/models.py
@@ -1,12 +1,11 @@
 
 from django.db import models
 
-from customer.models import Customer
+
 from product.models import Product
 
 class Category(models.Model):
     title = models.CharField(verbose_name= '', max_length=255)
-    customer = models.ManyToManyField(verbose_name='Производитель', to=Customer, related_name='category')
     country = models.CharField(verbose_name='Страна производства', max_length=255)
     photo = models.ImageField(verbose_name='Фото', upload_to='category')
     product = models.ManyToManyField(verbose_name='Продукт', to=Product, related_name='category')
--- product/models.py
+++ product/models.py
@@ -1,10 +1,9 @@
 
 from django.db import models
-from customer.models import Customer
+
 
 class Product(models.Model):
     title = models.CharField(verbose_name='Наименование', max_length=255)
-    customer = models.ManyToManyField(vebose_name ='Производитель', to=Customer, related_name='products')
     photo = models.ImageField(verbose_name='Фото', upload_to='products')
     description = models.TextField(verbose_name='Описание')
     ingridients = models.TextField(verbose_name='Состав')
