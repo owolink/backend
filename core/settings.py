af5d557ffc0fb635bb7d6cbf80cf7f02c4179089
ustinov.ivan565 <ustinov.ivan565@gmail.com>
2022-07-10 22:43:55 +0300

    fixed settings

--- core/settings.py
+++ core/settings.py
@@ -91,12 +91,8 @@ AUTH_USER_MODEL = 'authentify.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysqlite',
-        # 'NAME': '', 
-        # 'USER': '', 
-        # 'PASSWORD': '', 
-        # 'HOST': 'localhost', 
-        # 'PORT': '', 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
